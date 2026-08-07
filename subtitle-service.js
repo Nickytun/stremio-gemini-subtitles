457bfaab4b1614d97307db49a2b9cd6023092be7
Nickytun <135973985+Nickytun@users.noreply.github.com>
2026-08-07 13:41:33 +0700

    Update subtitle-service.js

--- subtitle-service.js
+++ subtitle-service.js
@@ -29,9 +29,9 @@ const JOB_TTL_SECONDS = DEFAULT_JOB_TTL_SECONDS;
 
 // KHAI BÁO MẢNG CHỨA CÁC API KEY GEMINI CỦA ÔNG Ở ĐÂY
 const GEMINI_API_KEYS = [
-    "KEY_SO_1_CUA_ONG",
-    "KEY_SO_2_CUA_ONG",
-    "KEY_SO_3_CUA_ONG"
+    "AIzaSyCdfbKv5o92Grr25LlPc68CyRYLj0alvLg",
+    "AIzaSyCtA0zTe1udpJoDSnJL0V64P_cAxWV76sk",
+    "AIzaSyANZDfp-r6HTZ9BurU5rVj039DszXvc7d0"
 ];
 
 const jobs = new LRUCache({
